999d344f687db0021451b669c7a21e8702e41252
Natan Pires <natan.void@hotmail.com>
2025-03-24 21:16:11 -0300

    feat: add email consumer

--- src/main/kotlin/com/email/consumers/EmailConsumer.kt
+++ src/main/kotlin/com/email/consumers/EmailConsumer.kt
@@ -1,15 +1,16 @@
 package com.email.consumers
 
 import com.email.dtos.EmailDto
+import com.email.services.EmailService
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 
 @Component
-class EmailConsumer {
+class EmailConsumer(private val emailService: EmailService) {
 
     @RabbitListener(queues = ["\${broker.queue.email.name}"])
     fun listenEmailQueue(@Payload emailDto: EmailDto) {
-        println(emailDto.userName)
+        emailService.sendEmail(emailDto)
     }
 }
